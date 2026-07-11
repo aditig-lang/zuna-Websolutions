aa141f1b6120d728bde096ff2381c1b0631fd831
aditig-lang <agupta4501@gmail.com>
2026-07-11 13:43:22 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,9 +3,9 @@ import styles from './Contact.module.css'
 
 const CONTACT_INFO = {
   founder: 'Aditi Gupta',
-  phone: '9279196754',
+  phone: '+91 9279196754',
   email: 'zunawebsolutions@gmail.com',
-  address: 'Varanasi, Uttar Pradesh, India',
+  address: 'Lanka, Varanasi, 221005, Uttar Pradesh, India',
 }
 
 export default function Contact() {
