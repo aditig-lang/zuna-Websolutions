6e0b6d0a35fddf09013ed0a42830d1767860e858
aditig-lang <agupta4501@gmail.com>
2026-07-12 15:54:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+server: {
+    proxy: {
+      // This tells Vite: if a request starts with '/api',
+      // forward it to your backend at localhost:3001
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
